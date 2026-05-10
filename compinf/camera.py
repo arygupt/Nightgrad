425547f09934d2d9941d0bb8bcd12ce1d8b0bd4c
Aryan <aryan@gupta-inc.com>
2026-05-09 23:59:25 -0400

    finding proper camera

--- compinf/camera.py
+++ compinf/camera.py
@@ -1,35 +1,36 @@
-from queue import Empty
-from zoneinfo import available_timezones
 import cv2 as cv
-import glob
-import os
-from cv2_enumerate_cameras import enumerate_cameras
-
+import subprocess
 
 def get_available_cameras():
     available_cameras = []
-    for i in range(5):
+    for i in range(2):
         cap = cv.VideoCapture(i)
         if cap.isOpened():
             available_cameras.append(i)
-            width = cap.get(cv.CAP_PROP_FRAME_WIDTH)
-            height = cap.get(cv.CAP_PROP_FRAME_HEIGHT)
-            print(width, height)
+            cap.release()
     return available_cameras
-
-'''
-def cameras_generator(ca=cv.CAP_ANY):
-    for path in glob.glob('/dev/video*'):
-        device_name = os.path.basename(path):
-'''          
-
 cameras = get_available_cameras()
 
-if cameras != Empty:
+# hacky way to get camera name
+def cam_list():
+    cam = []
+    result = subprocess.run(
+        ["ffmpeg", "-f", "avfoundation", "-list_devices", "true", "-i", ""],
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        text=True
+    )
+    
+    for line in result.stderr.splitlines():
+        if "Camera" in line:
+            cam.append(line.split("] ")[-1])
+
+    for i,x in enumerate(cam):
+        print(i, f"{x}")
+
+if cameras:
     print(f"Available cameras: {cameras}")
+    cam_list()
 else:
     print("No cameras found")
-
-for camera_info in enumerate_cameras():
-    print(f'{camera_info.index - 1200}: {camera_info.name}')
-
+    
